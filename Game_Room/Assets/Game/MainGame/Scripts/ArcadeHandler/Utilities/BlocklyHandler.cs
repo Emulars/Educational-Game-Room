8c937a89519e5fa2ec396586bad583af41188234
Emulars <davide.miggiano.majo@gmail.com>
2022-08-27 12:42:17 +0200

    Metroidvania and Puzzle blockly adjustments

--- Game_Room/Assets/Game/MainGame/Scripts/ArcadeHandler/Utilities/BlocklyHandler.cs
+++ Game_Room/Assets/Game/MainGame/Scripts/ArcadeHandler/Utilities/BlocklyHandler.cs
@@ -4,6 +4,7 @@ using Assets.DM.Script.Puzzle;
 using Assets.Scripts;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Random = System.Random;
 
 /* Build index
     
@@ -70,7 +71,13 @@ public class BlocklyHandler : MonoBehaviour
 
             // Second Puzzle interaction
             case 15:
+                Random rnd = new Random();
+                int bullets = rnd.Next(0, 101);  // creates a number between 0 and 100
+                int dragons = rnd.Next(1, 101);  // creates a number between 1 and 100
+
                 Executor.variabili.Add("result", "");
+                Executor.variabili.Add("numBullets", bullets.ToString());
+                Executor.variabili.Add("numDragons", dragons.ToString());
                 break;
 
             default:
@@ -137,14 +144,14 @@ public class BlocklyHandler : MonoBehaviour
     {
         // To move the character the speed should be higher than 0
 
-        if (Executor.variabili.Count != 0)
+        if (Executor.variabili.Count == 0) return;
+        if (!float.TryParse(Executor.variabili["speed"], out float speed)) return;
+        
+        if (speed is > 0f and <= 10f)
         {
-            if (Executor.variabili["speed"] != null)
-            {
-                float speed = float.Parse(Executor.variabili["speed"]);
-                bindedObject.GetComponent<PlayerMovement>().speed = speed;
-                Debug.Log("Metroidvania Level 1: \nSpeed = " + speed);
-            }
+            // Move the player
+            bindedObject.GetComponent<PlayerMovement>().speed = speed;
+            Debug.Log("Metroidvania Level 1: \nSpeed = " + speed);
         }
     }
 
@@ -152,77 +159,75 @@ public class BlocklyHandler : MonoBehaviour
     {
         // To make the jump possible, the speed must be equal to 1 and "enableJump" is set to true
 
-        if (Executor.variabili.Count != 0)
+        if (Executor.variabili.Count == 0) return;
+        if (!bool.TryParse(Executor.variabili["enableJump"], out var enableJump) ||
+            !float.TryParse(Executor.variabili["speed"], out var speed)) return;
+        if (speed == 1f && enableJump)
         {
-            if (Executor.variabili["enableJump"] != null && Executor.variabili["speed"] != null)
-            {
-                float speed = float.Parse(Executor.variabili["speed"]);
-                bool enableJump = bool.Parse(Executor.variabili["enableJump"]);
-                bindedObject.GetComponent<PlayerMovement>().speed = speed;
-                bindedObject.GetComponent<PlayerMovement>().enableJump = enableJump;
-                Debug.Log("Metroidvania Level 2: \nSpeed = " + speed);
-                Debug.Log("Metroidvania Level 2: \nEnable Jump = " + enableJump);
-            }
+            // Jump the player
+            bindedObject.GetComponent<PlayerMovement>().speed = speed;
+            bindedObject.GetComponent<PlayerMovement>().enableJump = enableJump;
+            Debug.Log("Metroidvania Level 2: \nSpeed = " + speed);
+            Debug.Log("Metroidvania Level 2: \nEnable Jump = " + enableJump);
         }
     }
 
     private void MetroidvaniaLevel3()
     {
-        if (Executor.variabili.Count != 0)
-        {
-            if (Executor.variabili["damage"] != null)
-            {
-                float damage = float.Parse(Executor.variabili["damage"]);
-                bindedObject.transform.Find("AttackTrigger").GetComponent<AttackTrigger>().damage = damage;
-                Debug.Log("Metroidvania Level 3: \nDamage = " + damage);
-            }
-        }
+        if (Executor.variabili.Count == 0) return;
+        if (!float.TryParse(Executor.variabili["damage"], out var damage)) return;
+        // Deal damage to the enemy
+        bindedObject.transform.Find("AttackTrigger").GetComponent<AttackTrigger>().damage = damage;
+        Debug.Log("Metroidvania Level 3: \nDamage = " + damage);
     }
 
     private void PuzzleQuest1()
     {
-        if (Executor.variabili.Count != 0)
+        if (Executor.variabili.Count == 0) return;
+        if (Executor.variabili["result"].Equals("1 sheep...2 sheep...3 sheep..."))
         {
-
-            if (Executor.variabili["result"].Equals("1 sheep...2 sheep...3 sheep..."))
+            // If the result is correct, check if the player is near the quest giver
+            if (bindedObject.GetComponent<QuestGiver>().playerIsClose)
             {
-                // If the result is correct, check if the player is near the quest giver
-                if (bindedObject.GetComponent<QuestGiver>().playerIsClose)
-                {
-                    // disable the first quest giver 
-                    bindedObject.SetActive(false);
-                    // enable the second one
-                    questGiver2.SetActive(true);
-                }
+                // disable the first quest giver 
+                bindedObject.SetActive(false);
+                // enable the second one
+                questGiver2.SetActive(true);
             }
-            else
-            {
-                // Change Quest Giver Dialog
-                Array.Clear(bindedObject.GetComponent<QuestGiver>().dialogue, 0, bindedObject.GetComponent<QuestGiver>().dialogue.Length);
-                bindedObject.GetComponent<QuestGiver>().dialogue[0] = "You need to return the murmur \'1 sheep...2 sheep...3 sheep...\' to complete this quest";
-                bindedObject.GetComponent<QuestGiver>().dialogue[1] = "Try to use the available variables to complete the sentence";
-                bindedObject.GetComponent<QuestGiver>().dialogue[2] = "Maybe a counting variable should be useful...";
-                bindedObject.GetComponent<QuestGiver>().dialogue[3] = "And a while loop can be useful...";
-                bindedObject.GetComponent<QuestGiver>().dialogue[4] = "Press F1 to open the Block UI";
+        }
+        else
+        {
+            // Change Quest Giver Dialog
+            Array.Clear(bindedObject.GetComponent<QuestGiver>().dialogue, 0, bindedObject.GetComponent<QuestGiver>().dialogue.Length);
+            bindedObject.GetComponent<QuestGiver>().dialogue[0] = "You need to return the murmur \'1 sheep...2 sheep...3 sheep...\' to complete this quest";
+            bindedObject.GetComponent<QuestGiver>().dialogue[1] = "Try to use the available variables to complete the sentence";
+            bindedObject.GetComponent<QuestGiver>().dialogue[2] = "Maybe a counting variable should be useful...";
+            bindedObject.GetComponent<QuestGiver>().dialogue[3] = "And a while loop can be useful...";
+            bindedObject.GetComponent<QuestGiver>().dialogue[4] = "Press F1 to open the Block UI";
 
-                // Reset Variable
-                Executor.variabili["word"] = "sheep";
-                Executor.variabili["space"] = " ";
-                Executor.variabili["result"] = "";
-            }
-        
+            // Reset Variable
+            Executor.variabili["word"] = "sheep";
+            Executor.variabili["space"] = " ";
+            Executor.variabili["result"] = "";
         }
     }
 
     private void PuzzleQuest2()
     {
-        if (Executor.variabili.Count != 0)
+        if (Executor.variabili.Count == 0) return;
+        if (!bool.TryParse(Executor.variabili["result"], out var result)) return;
+
+        if (!float.TryParse(Executor.variabili["numBullets"], out var nBullet) ||
+            !float.TryParse(Executor.variabili["numDragons"], out var nDragon)) return;
+        
+        Debug.Log("Puzzle Quest 2: \nResult = " + result);
+        Debug.Log("Puzzle Quest 2: \nBullets = " + nBullet);
+        Debug.Log("Puzzle Quest 2: \nDragons = " + nDragon);
+        var operation = ((nBullet / 2) - nDragon >= 0) ? true : false;
+        if ( (operation && result) || (!operation && !result))
         {
-            if (Executor.variabili["result"].Equals("true"))
-            {
-                if (bindedObject.GetComponent<QuestGiver>().playerIsClose)
-                    bindedObject.SetActive(false);  // disable quest giver 
-            }
+            if (bindedObject.GetComponent<QuestGiver>().playerIsClose)
+                bindedObject.SetActive(false);  // disable quest giver 
         }
     }
 }
